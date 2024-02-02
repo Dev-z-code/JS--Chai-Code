c3ad4927875232baba76f6a15c053e429b762e42
Dev-z-code <64522000+Dev-z-code@users.noreply.github.com>
2024-02-02 14:36:36 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,23 @@ let accountEmail = "sachin@gmauil.com"
 var accountPassword = "123565"
 accountCity= "Lucknow"
 
-accountId = 1111111
-console.log(accountId)
+let accountState;
+
+// accountId = 1111111 not allowed to change
+// accountEmail="abc@gmail.com" allowed to change , latter one willbe fetched
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+
+*/
+
+/*
+if you declare a variable and doesnt declare value / it'll shows undefined
+*/
+accountPassword="111111"
+accountCity="Jaipur"
+console.log(accountPassword)
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 

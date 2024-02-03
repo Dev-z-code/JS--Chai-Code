7cbbeb21635cb1180bd0e18e84dd52614a2cfb63
Dev-z-code <64522000+Dev-z-code@users.noreply.github.com>
2024-02-03 05:27:45 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,49 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(3
+    +
+    3) 
+
+    // code readibility should be high
+
+
+console.log("Sachin")
+
+
+let name="Sachin" 
+
+// strign datType
+
+let age=18
+
+// number dataType => 2 to power 53
+
+let isLoggedIn=true
+
+let state;
+
+// boolean dataType => true/false
+
+// null => standalone value => value is defined null
+
+// undefined=> value is not defined
+
+// symbol=> unique
+
+
+
+// obejct 
+
+console.log(typeof age); 
+
+// it will tell what datatypes it is
+
+ console.log(typeof null);
+
+// it is object datatype
+
+console.log(typeof undefined);
+
+// it is undefined dataType

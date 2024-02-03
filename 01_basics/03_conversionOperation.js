51b5c59a4af9cd952aa4471474c7e88ab0f0d802
Dev-z-code <64522000+Dev-z-code@users.noreply.github.com>
2024-02-03 12:48:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+// let score = false
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+let age = false
+
+let x = Number(age)
+
+//  console.log(typeof x)
+
+//  console.log(x)
+
+// console.log(typeof age);
+
+/*
+
+"33" => 33
+"33abc" => Nan //Not A Number
+ture =>1; false =>0 
+
+
+*/
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+//  1=> ture; 0=> false
+// "" => false
+//  "sachin" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);

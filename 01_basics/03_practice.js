910597843ac3e212f3cc54aae655a745a90b32b8
Dev-z-code <64522000+Dev-z-code@users.noreply.github.com>
2024-02-04 08:47:46 +0000

    practice

--- 01_basics/03_practice.js
+++ 01_basics/03_practice.js
@@ -5,4 +5,23 @@ let age = "undefined"
 let ageInNumber = Number(age)
 
 // console.log(typeof ageInNumber);
-console.log(ageInNumber);
+// console.log(ageInNumber);
+
+let code1 = "Hello"
+let code2 = " Sachin"
+let code3 = " This is your first JS Code."
+
+let code4 = code1 + code2 + code3
+
+// console.log(code1 + code2 + code3);
+// console.log(code4);
+
+// console.log( "2" + 2);
+
+// number  within quotes "" treated as string not number 
+
+// console.log( "2" + 2 + 3);
+
+// console.log(3 + 1);
+
+console.log(Hello World);

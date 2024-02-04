4434a8fa1942b1677ca0369a1623e40f0cb49c89
Dev-z-code <64522000+Dev-z-code@users.noreply.github.com>
2024-02-04 08:13:53 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -45,6 +45,19 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************Operations*********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(2+2)
+console.log(6*3);
+console.log(6/2);
+console.log(2**3);
+console.log(3**2);
+console.log(2%3);

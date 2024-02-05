261dbe933cf32c70d746bb432e4c84f1d35ef4c9
Dev-z-code <64522000+Dev-z-code@users.noreply.github.com>
2024-02-05 18:08:24 +0000

    summary done

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,26 @@
+// Primitive
+
+
+// 7 Types : String, Number , Boolean,  null, Boolean, Symbol, BigInt
+
+
+const userName = "Sachin"
+const userAge = 21
+const isLoggedIn = false
+const activityDone = null
+let formFillUp ;
+
+const id = symbol('123')
+const anotherId = symbol ('123')
+
+console.log(id == anotherId);
+console.log(formFillUp);
+
+const bigNumber = 2154531848n
+
+
+// Non Premitive --Reference Type
+// Array, Objects, Functions
+
+
+

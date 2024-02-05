873fe08ec7cdc8ead3e6c4d839a951eea8e9b3d0
Dev-z-code <64522000+Dev-z-code@users.noreply.github.com>
2024-02-05 17:39:38 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log(2 > 2);
+// console.log(2 == 2);
+// console.log(4 >= 3);
+// console.log(9 != 6);
+
+// console.log( "8" > 7);
+
+console.log( null > 3);
+console.log(null != 0);
+
+console.log( "2" === 2);
